0f5ed679801316389bc3d161e205c43080e95105
webmaster254 <josephm2800@gmail.com>
2019-02-07 21:26:07 +0300

    [feature #163722797]added test to get all parties

--- app/api/v1/views/party_view.py
+++ app/api/v1/views/party_view.py
@@ -33,8 +33,12 @@ def parties():
                 "status": "Unprocessable Entity",
                 "error": "Empty data field"
             }), 422
-        else:
-            custom_response = jsonify(sample_party.create_party()), 201
+            else:
+                custom_response = jsonify(sample_party.create_party()), 201
+
+    elif request.method == "GET":
+        custom_response = jsonify(PParties.get_all_parties())
+
     else:
         pass
 
